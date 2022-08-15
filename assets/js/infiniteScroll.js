ab74d603f3c1e9420cd4444510e9a8e68738f574
abigailojeda <abigail.arte@gmail.com>
2022-08-15 18:28:29 +0100

    fix scroll

--- assets/js/infiniteScroll.js
+++ assets/js/infiniteScroll.js
@@ -1,16 +1,20 @@
 const scrollCont = document.querySelector('.scroll-cont');
 const scrollContent = document.querySelector('.scroll');
-const scollBottom = document.querySelector('.scroll-bottom-border')
-
+const gallerys = document.querySelectorAll('.gallery-cont');
 
 scrollCont.addEventListener("scroll", ()=>{
     let totalHeight= scrollContent.clientHeight;
+    console.log('tope: ', gallerys[0].getBoundingClientRect().top)
     //console.log(scrollContent.clientHeight)
-    console.log(scollBottom.getBoundingClientRect().top)
-    if(scollBottom.getBoundingClientRect().top<=920){
+    // console.log(scollBottom.getBoundingClientRect().top)
+    if(gallerys[0].getBoundingClientRect().top<=-904){
         scrollContent.style.height = totalHeight + 2400 + "px";
+        const node= scrollContent.getElementsByClassName('gallery-cont')[0];
+        const clone = node.cloneNode(true);
+        console.log(node)
+       // console.log(clone)
         // const newDiv = document.createElement("div");
         // newDiv.setAttribute('class', 'scroll')
-        // scrollCont.appendChild(newDiv)
+        scrollContent.appendChild(clone)
     }
 })
